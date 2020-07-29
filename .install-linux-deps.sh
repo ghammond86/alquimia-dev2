01ff438fb7808588591f3421c0ff188018d472d1
Glenn Hammond <glenn.hammond@pnnl.gov>
2020-07-29 15:57:13 -0700

    updated pflotran install to use v3.0

--- .install-linux-deps.sh
+++ .install-linux-deps.sh
@@ -7,10 +7,17 @@ export TMPDIR=/tmp
 #git clone https://bitbucket.org/petsc/petsc petsc
 git clone https://gitlab.com/petsc/petsc.git petsc
 pushd $PETSC_DIR
-git checkout v3.11.3
-./configure --with-mpi=1 --download-hdf5 --with-debug=$DEBUG --with-shared-libraries=1 --download-pflotran --download-pflotran-commit=origin/master
+git checkout v3.13
+./configure --with-mpi=1 --download-hdf5 --with-debug=$DEBUG --with-shared-libraries=1
 make
 ##popd
+
+# clone and build PFLOTRAN
+cd $PETSC_ARCH/externalpackages
+git clone git@bitbucket.org:pflotran/pflotran.git git.pflotran
+cd git.pflotran/src/pflotran
+git checkout v3.0
+make libpflotranchem.a
 
 # Get and build CrunchTope, inside external packages for convenience
 cd $PETSC_ARCH/externalpackages
