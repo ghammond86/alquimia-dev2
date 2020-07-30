c0379226984ace0afeec6a8b0a8e97308d882ffc
Glenn Hammond <glenn.hammond@pnnl.gov>
2020-07-30 15:35:02 -0700

    yet another

--- .travis/install-petsc.sh
+++ .travis/install-petsc.sh
@@ -3,6 +3,5 @@
 git clone https://gitlab.com/petsc/petsc.git $PETSC_DIR
 cd $PETSC_DIR
 git checkout v3.11.3
-#./configure --with-mpi=1 --download-hdf5 --with-debug=$DEBUG --with-shared-libraries=1 --download-fblaslapack=1
-./configure --with-mpi=1 --with-debug=1 --with-shared-libraries=1 --download-fblaslapack=1
+./configure --with-mpi=1 --with-debug=1 --with-shared-libraries=1 --download-fblaslapack=1 --with-debug=$DEBUG
 make
