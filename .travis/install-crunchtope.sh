106fa6cafbb1b729dd40a8a7c19cf409f78b0e2d
Glenn Hammond <glenn.hammond@pnnl.gov>
2020-07-30 12:51:24 -0700

    fixed paths

--- .travis/install-crunchtope.sh
+++ .travis/install-crunchtope.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git clone https://bitbucket.org/crunchflow/crunchtope-dev.git --branch petsc-upgrade $THIRD_PARTY_LIB_DIR
+git clone https://bitbucket.org/crunchflow/crunchtope-dev.git --branch petsc-upgrade $THIRD_PARTY_LIB_DIR/crunchtope-dev
 cd $THIRD_PARTY_LIB_DIR/crunchtope-dev/source
 git apply MakefileForAlquimia.patch
 make libcrunchchem.a
--- .travis/install-pflotran.sh
+++ .travis/install-pflotran.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git clone https://bitbucket.org/pflotran/pflotran $THIRD_PARTY_LIB_DIR
+git clone https://bitbucket.org/pflotran/pflotran $THIRD_PARTY_LIB_DIR/pflotran
 cd $THIRD_PARTY_LIB_DIR/pflotran/src/pflotran
 git checkout v3.0
 make libpflotranchem.a
